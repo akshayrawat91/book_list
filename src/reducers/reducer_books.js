0110cf1f4869efdb4b1a3f9cb4fd07c0bd4c0f1e
Akshay <akxrawat@gmail.com>
2018-07-21 17:51:52 -0500

    added book_selected action

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,7 +1,7 @@
 export default function() {
     return [
-        {title: 'Javascript: the good parts'},
-        {title: 'Harry potter'},
-        {title: 'Dark tower'}
+        {title: 'Javascript: the good parts', pages: 101},
+        {title: 'Harry potter', pages: 39},
+        {title: 'Dark tower', pages: 50}
     ];
 }
